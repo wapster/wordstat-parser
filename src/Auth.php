060b536df859d19f6452c157fb58d4114b83813d
r0ma <mymail@nxt.ru>
2021-10-18 15:49:21 +0500

    Добавлен метод авторизации на сайте

--- src/Auth.php
+++ src/Auth.php
@@ -16,7 +16,23 @@ class Auth extends Driver
             $driver->findElement(WebDriverBy::xpath("//*[text()='Войти']"))->click();
             sleep(1);
 
-            
+            $login_input = $driver->findElement(WebDriverBy::id("b-domik_popup-username"));
+            $password_input = $driver->findElement(WebDriverBy::id("b-domik_popup-password"));
+
+            $login_input->sendKeys($login);
+            sleep(1);
+            $password_input->sendKeys($password);
+            sleep(1);
+
+            $submit_button = $driver->findElement(WebDriverBy::xpath("/html/body/form/table/tbody/tr[2]/td[2]/div/div[5]/span[1]/input"));
+            $submit_button->click();
+            sleep(3);
+
+            $log_out_link = $driver->findElements(WebDriverBy::xpath("//*[text()='Выход']"));
+            if (count($log_out_link) > 0) {
+                return true;
+            } else return false;
+
         } catch (Exception\WebDriverException $e) {
             return false;
             $driver->quit();
