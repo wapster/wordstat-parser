ccd4f83c7b387171458ccb5dd0b160cdc6873c75
r0ma <mymail@nxt.ru>
2021-10-18 15:26:23 +0500

    Добавлен метод получения Аккаунта из файла с аккаунтами

--- src/Accounts.php
+++ src/Accounts.php
@@ -1,1 +1,17 @@
 <?php
+
+class Accounts 
+{
+
+    public function getAccount(): array {
+        $accounts = file('config/accounts.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        $counter = count($accounts);
+        $index = rand(0, $counter - 1);
+        $account = $accounts[$index];
+        
+        $account = explode(";", $account);
+        return $account; // [0] - login, [1] - password
+    }
+
+}
