0a98f57a241a3291af80df822263bff687f11116
r0ma <mymail@nxt.ru>
2021-10-21 20:47:42 +0500

    изменен формат возвращаемых данных. Теперь метод обрабатывает данные из json формата, раньше был просто тестовый файл, в котором каждая строка это ip адрес прокси сервера и его порт

--- src/Proxy.php
+++ src/Proxy.php
@@ -4,11 +4,11 @@ class Proxy
 {
 
     public function getProxy(): string {
-        $proxys = file('config/proxy.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        $counter = count($proxys);
-        $index = rand(0, $counter - 1);
-        $proxy = $proxys[$index];
-        return $proxy; // format <ip:port>
+        $file = file_get_contents('config/proxy.txt');
+        $proxys = json_decode($file, true);
+        $ip = array_rand($proxys);
+        $port = $proxys[$ip];
+        return $ip . ":" . $port; // format <ip:port>
     }
 
 }
